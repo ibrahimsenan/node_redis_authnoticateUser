75cfda113849cc1f9c7a47f4c169d544528bab07
Ibrahim <ibrahim.senan@yahoo.com>
2021-11-18 20:33:47 +0100

    Create user dcoument Api Service/Handlers

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 const USER_AUTH_API = require("../apis/userAuth/auth");
+const USER_DOCUMENTS = require("../apis/userDocuments/documents");
 
-const APP_ROUTES = [USER_AUTH_API];
+const APP_ROUTES = [USER_AUTH_API, USER_DOCUMENTS];
 
 module.exports = APP_ROUTES;
