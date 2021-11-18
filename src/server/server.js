69d2e2c51f7ab3dc11ce290043022476a3055636
Ibrahim <ibrahim.senan@yahoo.com>
2021-11-18 20:35:06 +0100

    enhance server

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,4 @@
-const express = require("express");
-var Logger = require("morgan");
-const server = express();
 const PORT = 6700;
-var bodyParser = require("body-parser");
 require("../redis/redis");
 
 //-------------------------------------------
@@ -22,15 +18,13 @@ const CREAT_ROUTE = require("../routes/routes");
 //-------------------------------------------
 //          EXPRESS SERVER
 //------------------------------------------
+const SERVER_EX = require("./express");
+SERVER_EX.use("/api/v1/", CREAT_ROUTE);
 
-server.get("/", function (req, res) {
-  res.send("<br/><h1>User Auth Service Started!</h1> \n <br />");
-});
+//-------------------------------------------
+//          RUNNING SERVER
+//------------------------------------------
 
-server.use(bodyParser.json());
-server.use(Logger("dev"));
-server.use("/api/v1/", CREAT_ROUTE);
-
-server.listen(PORT, () => {
-  console.log("Auth Service Started at:", "http://localhost:5000");
+SERVER_EX.listen(PORT, () => {
+  console.log("Auth Service Started at:", `http://localhost:${PORT}`);
 });
